55f1f8e70d5bdb930ed50b2b65d38174d3687ee9
Piers Thompon <pthom198@MGC000009767.warley.ford.com>
2019-07-15 18:01:05 +0100

    comments and users working and appearing on show post in LIST FORM

--- app/src/main/java/com/fotg/keepingcool/CommentAdapter.java
+++ app/src/main/java/com/fotg/keepingcool/CommentAdapter.java
@@ -7,16 +7,30 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+
 import com.fotg.keepingcool.models.Comment;
 import com.fotg.keepingcool.models.Post;
+import com.fotg.keepingcool.models.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class CommentAdapter extends BaseAdapter {
 
+    private FirebaseDatabase db = FirebaseDatabase.getInstance();
+    final DatabaseReference postRef = db.getReference("/posts");
+    final DatabaseReference userRef = db.getReference("/users");
+
     LayoutInflater mInflater;
     ArrayList<Comment> items;
     Context context;
+    User user;
+    String name;
 
 
     public CommentAdapter(Context c, ArrayList<Comment> ps) {
@@ -45,10 +59,27 @@ public class CommentAdapter extends BaseAdapter {
         View v = mInflater.inflate(R.layout.activity_comments_list_view, null);
 
         TextView commentTextView = v.findViewById(R.id.commentID);
+        TextView userTextView = v.findViewById(R.id.userComment);
 
         String comments = items.get(i).getComment();
+        String userComments = items.get(i).getuID();
+
 
         commentTextView.setText(comments);
+//        userTextView.setText(userComments);
+
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                user = dataSnapshot.child(userComments).getValue(User.class);
+                name = user.getName();
+                userTextView.setText(name);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
 
         return v;
     }
