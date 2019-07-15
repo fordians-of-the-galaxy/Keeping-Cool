c806f88b1530dba37e16205311d33b3fdf9efd6f
taneejaram <taneeja.ram2@hotmail.com>
2019-07-15 14:31:05 +0100

    Created davids first tip (incomplete)

--- app/src/main/java/com/fotg/keepingcool/DavidsTipsActivity.java
+++ app/src/main/java/com/fotg/keepingcool/DavidsTipsActivity.java
@@ -10,17 +10,13 @@ import android.widget.ImageView;
 public class DavidsTipsActivity extends AppCompatActivity {
 
     ImageView babysealBackground;
-    Animation babysealAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_davids_tips);
 
-        babysealAnim = AnimationUtils.loadAnimation(this, R.anim.babyseal_anim);
-
         babysealBackground = findViewById(R.id.babysealBackground);
 
-        babysealBackground.animate().translationY(-430).setDuration(0);
     }
 }
