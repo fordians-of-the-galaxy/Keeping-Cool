0cd5bcb02a63b9a42cc7b2ddcee7dff140ffd77a
Piers Thompon <pthom198@MGC000009767.warley.ford.com>
2019-07-16 17:16:17 +0100

    handy links initial UI

---
+++ app/src/main/java/com/fotg/keepingcool/HandyLinksActivity.java
@@ -0,0 +1,16 @@
+package com.fotg.keepingcool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HandyLinksActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_handy_links);
+    }
+}
