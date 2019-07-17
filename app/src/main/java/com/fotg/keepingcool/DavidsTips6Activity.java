cdceb8d94445c014ba38d6c16761684e76fbe8ea
adrem7 <adrian.raisin@gmail.com>
2019-07-17 12:19:04 +0100

    nav bar fixed using breaks, also nav added to create comments post

--- app/src/main/java/com/fotg/keepingcool/DavidsTips6Activity.java
+++ app/src/main/java/com/fotg/keepingcool/DavidsTips6Activity.java
@@ -42,15 +42,19 @@ public class DavidsTips6Activity extends AppCompatActivity {
                     case R.id.news_feed:
                         Intent feed_intent = new Intent(getApplicationContext(), ListPostsActivity.class);
                         startActivity(feed_intent);
+                        break;
 //                    case R.id.useful_links:
 //                        Intent links_intent = new Intent(getApplicationContext(), UsefulLinksActivity.class);
 //                        startActivity(links_intent);
+    //                    break;
 //                    case R.id.calendar:
 //                        Intent events_intent = new Intent(getApplicationContext(), EventsActivity.class);
 //                        startActivity(events_intent);
+//                        break;
 //                    case R.id.bindr:
 //                        Intent bindr_intent = new Intent(getApplicationContext(), BindrActivity.class);
 //                        startActivity(bindr_intent);
+//                        break;
                 }
                 return true;
             }
--- app/src/main/java/com/fotg/keepingcool/ListPostsActivity.java
+++ app/src/main/java/com/fotg/keepingcool/ListPostsActivity.java
@@ -56,15 +56,19 @@ public class ListPostsActivity extends ToolbarActivity {
                     case R.id.tips:
                         Intent tips_intent = new Intent(getApplicationContext(), DavidsTipsActivity.class);
                         startActivity(tips_intent);
+                        break;
 //                    case R.id.useful_links:
 //                        Intent links_intent = new Intent(getApplicationContext(), UsefulLinksActivity.class);
 //                        startActivity(links_intent);
+//                        break;
 //                    case R.id.calendar:
 //                        Intent events_intent = new Intent(getApplicationContext(), EventsActivity.class);
 //                        startActivity(events_intent);
+//                        break;
 //                    case R.id.bindr:
 //                        Intent bindr_intent = new Intent(getApplicationContext(), BindrActivity.class);
 //                        startActivity(bindr_intent);
+//                        break;
                 }
                 return true;
             }
