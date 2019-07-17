59b72175d1f5bcabc56be52f0e2e9524f000c242
Egle Bernotaite <ebernotaite94@gmail.com>
2019-07-17 11:09:07 +0100

    Upvote button changes colour when user upvotes

--- app/src/main/java/com/fotg/keepingcool/ShowPostActivity.java
+++ app/src/main/java/com/fotg/keepingcool/ShowPostActivity.java
@@ -3,6 +3,7 @@ package com.fotg.keepingcool;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import com.fotg.keepingcool.models.Comment;
 import com.fotg.keepingcool.models.Post;
@@ -260,7 +261,13 @@ public class ShowPostActivity extends AppCompatActivity {
                     String key = upvote.getKey();
                     upvotes.put(vote, key);
                 }
+                if(upvotes.containsKey(uid)) {
+                    upvoteButton.setColorFilter(Color.parseColor("#00c2c7"));
+                } else {
+                    upvoteButton.setColorFilter(Color.BLACK);
+                }
                 upvotesNumber.setText(upvotes.size() + " votes");
+
             }
 
             @Override
